137e5a84ea9fbf1dea3236edd427e291c4347e29
oti <otiext@gmail.com>
2020-07-20 22:58:33 +0900

    feat(script): JS 実装を追加した
    - ロード時にランダムでテーミングのクラスを付与する実装
    - ダークモードをトグルする実装

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,5 +1,21 @@
 // for IE11
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import { ThemeNames } from "./ThemeNames";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const ThemeName = ThemeNames[Math.floor(Math.random() * ThemeNames.length)];
+  const $html = document.documentElement;
+  const $body = document.body;
+  const $mode = document.querySelector("#Mode");
+
+  // body 要素にテーミングクラスを追加する
+  $body.classList.add(ThemeName);
+
+  // ダークモードのトグル
+  $mode.addEventListener(
+    "change",
+    () => $html.classList.toggle("-dark"),
+    false
+  );
+});
