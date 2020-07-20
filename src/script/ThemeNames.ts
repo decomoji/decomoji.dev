4b324feaf5d2a722bf7c8048ba1389679e30d438
oti <otiext@gmail.com>
2020-07-20 22:58:00 +0900

    feat(script): テーミングのクラス名の定義ファイルを追加した

---
+++ src/script/ThemeNames.ts
@@ -0,0 +1,14 @@
+export const ThemeNames: string[] = [
+  "-Tomato",
+  "-Chocolate",
+  "-Gold",
+  "-Olive",
+  "-Forest",
+  "-Teal",
+  "-Cerulean",
+  "-Denim",
+  "-Danube",
+  "-Lavender",
+  "-Azalea",
+  "-Coral",
+];
